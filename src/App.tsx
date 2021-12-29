7d44e257fad85c9bf9f2c3bae78c9372b4427fda
Falcon <falcon@Falcons-MacBook-Pro.local>
2021-12-29 16:05:31 +0800

    deploy

---
+++ src/App.tsx
@@ -0,0 +1,118 @@
+import Container from "react-bootstrap/Container";
+import Abstract from "./Abstract";
+import Publications from "./Publications";
+import WorkExperience from "./Work";
+import Education from "./Education";
+import TeachingExperience from "./Teaching";
+import Service from "./Service";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInbox,
+  faFile,
+  faUser,
+  faIdBadge,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
+  faGithub,
+  faLinkedin,
+  faMicrosoft,
+} from "@fortawesome/free-brands-svg-icons";
+import Button from "react-bootstrap/Button";
+//import "academicons/css/academicons.css";
+
+function Contact() {
+  return (
+    <>
+      <h3>
+        <FontAwesomeIcon icon={faInbox} /> Contact
+      </h3>
+      <h4>Work: kisubki{`m[at]s`}mu.edu.sg</h4>
+      <h4>Personal: falconlk00[a{`t]`}gmail.com</h4>
+      <Button href="https://github.com/FalconLK" variant="outline-info">
+        <FontAwesomeIcon icon={faGithub} /> Github
+      </Button>{" "}
+      <Button href="https://www.linkedin.com/in/kim-kisub-81199a179" variant="outline-secondary">
+        <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+      </Button>{" "}
+
+    </>
+  );
+}
+
+function BibliographicProfiles() {
+  return (
+    <>
+      <h3>
+        <FontAwesomeIcon icon={faIdBadge} /> Bibliographic Profiles{" "}
+      </h3>
+      <Button href="https://scholar.google.com/citations?user=Nr_IDzQAAAAJ&hl=ko" variant="outline-success">
+        <i className="ai ai-google-scholar" /> Google Scholar
+      </Button>{" "}
+      <Button href="https://dblp.org/pid/216/3065.html" variant="outline-success">
+        <i className="ai ai-dblp" /> DBLP
+      </Button>{" "}
+      <Button href="https://academic.microsoft.com/author/2794526229/publication/search?q=Kisub%20Kim&qe=Composite(AA.AuId%253D2794526229)&f=&orderBy=0&paperId=2795013376" variant="outline-success">
+        <FontAwesomeIcon icon={faMicrosoft} /> Microsoft Academic
+      </Button>{" "}
+      <Button href="https://orcid.org/0000-0002-4462-6916" variant="outline-success">
+        <i className="ai ai-orcid" /> ORCID
+      </Button>
+    </>
+  );
+}
+
+function CV() {
+  return (
+    <>
+      <h3>
+        <FontAwesomeIcon icon={faUser} /> Curriculum Vitae
+      </h3>
+      <Button
+        href="https://github.com/FalconLK/falconlk.github.io/CV.pdf"
+        variant="outline-success"
+      >
+        <FontAwesomeIcon icon={faFile} /> View my CV
+      </Button>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Abstract />
+      <Container>
+        <hr />
+        <Contact />
+        <hr />
+        <CV />
+        <hr />
+        <BibliographicProfiles />
+        <hr />
+        <Publications />
+        <hr />
+        <Education />
+        <hr />
+        <WorkExperience />
+        <hr />
+        <TeachingExperience />
+        <hr />
+        <Service />
+        <footer style={{ marginTop: "5rem", marginBottom: "3rem" }}>
+          <hr />
+          <p className="text-center">
+            &copy; 2021 Kisub Kim. The&nbsp;
+            <a href="https://github.com/handk85/react-homepage">
+              source code of this homepage
+            </a>
+            &nbsp;is available at Github by DK. Please feel free to extend it for your
+            own homepage.
+          </p>
+        </footer>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Publications.tsx
@@ -0,0 +1,152 @@
+import { useState, useEffect, ReactElement } from "react";
+import Spinner from "react-bootstrap/Spinner";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook, faClipboard } from "@fortawesome/free-solid-svg-icons";
+import { generateBibtex, getVenue, getAuthors } from "./Paper";
+import { Paper, PaperType } from "./Types";
+import { load } from "js-yaml";
+
+function generateLink(url: string, name: string): ReactElement {
+  return <a href={url}>[{name}]</a>;
+}
+
+function objToString(item: Paper): string {
+  const venue: string = getVenue(item);
+  const authors = getAuthors(item);
+  if (item.type === "thesis") {
+    return `"${item.title}", ${item.school}, ${item.year}`;
+  }
+  return `${authors}, "${item.title}", ${venue}, ${item.year}${
+    item.note ? `, ${item.note}` : ""
+  }`;
+}
+
+function BibtexModal(props: any) {
+  const [copied, setCopied] = useState(false);
+
+  const paper: Paper = props.paper;
+  const bibtex = generateBibtex(paper);
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Bibtex</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <pre>{bibtex}</pre>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          active={copied}
+          onClick={() => {
+            setCopied(true);
+            setTimeout(() => {
+              setCopied(false);
+            }, 3000);
+            navigator.clipboard.writeText(bibtex);
+          }}
+        >
+          <FontAwesomeIcon icon={faClipboard} />{" "}
+          {copied ? "Copied" : "Copy to clipboard"}
+        </Button>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function Papers(props: { papers: Paper[] }) {
+  const [modalShow, setModalShow] = useState(false);
+  const [bibtexPaper, setBibtexPaper] = useState<Paper>();
+  const { papers } = props;
+
+  return (
+    <>
+      <ul>
+        {papers.map((item) => {
+          return (
+            <li key={item.id}>
+              {objToString(item)} {item.site && generateLink(item.site, "SITE")}
+              {item.pdf && generateLink(`${item.pdf}`, "PDF")}
+              {item.DOI && generateLink(`https://doi.org/${item.DOI}`, "DOI")}
+              {item.type !== "thesis" && item.type !== "book" && (
+                <a
+                  href=""
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setModalShow(true);
+                    setBibtexPaper(item);
+                  }}
+                >
+                  [Bibtex]
+                </a>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+      {bibtexPaper && (
+        <BibtexModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          paper={bibtexPaper}
+        />
+      )}
+    </>
+  );
+}
+
+async function loadYaml(path: string) {
+  const response = await fetch(path);
+  const text = await response.text();
+  const data: Paper[] = (await load(text)) as Paper[];
+  data.sort((a: Paper, b: Paper) => {
+    return b.year - a.year;
+  });
+  return data;
+}
+
+function PaperGroup(props: { paperType: PaperType }) {
+  const [papers, setPapers] = useState<Paper[]>();
+  const { paperType } = props;
+
+  useEffect(() => {
+    (async function () {
+      const data = await loadYaml(`./data/${paperType}.yaml`);
+      data.forEach((item) => {
+        item.type = paperType;
+      });
+      setPapers(data);
+    })();
+  }, [paperType]);
+
+  return (
+    <>
+      <h4 style={{ textTransform: "capitalize" }}>{paperType}</h4>
+      {!papers ? <Spinner animation="border" /> : <Papers papers={papers} />}
+    </>
+  );
+}
+
+function Publications() {
+  return (
+    <>
+      <h3>
+        <FontAwesomeIcon icon={faBook} /> Publications
+      </h3>
+      <>
+        <PaperGroup paperType="journal" />
+        <PaperGroup paperType="conference" />
+        <PaperGroup paperType="thesis" />
+      </>
+    </>
+  );
+}
+
+export default Publications;
