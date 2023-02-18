21b2a4a5e8b0219f8f1e46c482e52b8a2dc80ec5
SMU <smu@MacBook-Pro.local>
2023-02-18 23:13:10 +0800

    deploy

--- src/Abstract.tsx
+++ src/Abstract.tsx
@@ -14,12 +14,12 @@ function Abstract() {
         </Col>
         <Col>
           <h2>Kisub Kim</h2>
-          <h4>Research Scientist, Singapore Management University</h4>
-          Dr. Kisub Kim is a research scientist (postdoc) at{" "}
+          <h4>Research Scientist at Singapore Management University</h4>
+          I am a research scientist (postdoc) at{" "}
           <a href="https://www.smu.edu.sg">
             Singapore Management University (SMU)
           </a>
-          . He is mainly working on software engineering, specifically application of artificial intelligence under the supervision of{" "}
+          . My work is mainly related to Software Engineering, broadly source code analysis and, specifically, code search, bug localization, code review, and code representation under the supervision of{" "}
           <a href="http://www.mysmu.edu/faculty/davidlo/">Prof. David Lo</a>.
           Before joining SMU, he was a research associate at the{" "}
           <a href="https://wwwen.uni.lu/">University of Luxembourg</a>. He completed
@@ -29,36 +29,9 @@ function Abstract() {
           <a href="https://bissyande.github.io/">
             Prof. Dr. Tegawendé F. BISSYANDE
           </a>
-          ,{" "}
-          <a href="https://www.darkrsw.net/">
-            Prof. Dr. Dongsun Kim
-          </a>
-          ,{" "}
-          <a href="https://jacquesklein2302.github.io/">
-            Prof. Dr. Jacques Klein
-          </a>
-          , and{" "}
-          <a href="https://wwwfr.uni.lu/snt/people/yves_le_traon">
-            Prof. Dr. Yves Le Traon
-          </a>
-          . He has worked for{" "}
-          <a href="https://www.khmc.or.kr/">
-            Kyung Hee University Hospital
-          </a>{" "}
-          as a developer. He received his M.Eng. at {" "}
-          <a href="http://www.chungbuk.ac.kr">
-            Chungbuk National University
-          </a>{" "}
-          under the supervision of{" "}
-          <a href="https://scholar.google.com.hk/citations?user=U7Tc7gEAAAAJ&hl=en">Prof. Dr. Woosung Jung</a>. He
-          received B.Eng. in{" "}
-          <a href="http://ce.jejunu.ac.kr">Computer Engineering Department</a>{" "}
-          at <a href="http://jejunu.ac.kr">Chungbuk National University</a> under
-          the supervision of{" "}
-          <a href="http://cybertec.jejunu.ac.kr/profile.html">
-            Prof. Dr. Younghun Seo
-          </a>
-          . His main research domain include code search, bug localization, and code representation.
+          .
+          My works were presented at major conferences such as International Conference on Software Engineering and published in top journals such as Empirical Software Engineering. 
+          I have served as a reviewer, program committee, and organization committee in various software engineering conferences and journals, such as IEEE Transactions on Software Engineering, Empirical Software Engineering, and IEEE/ACM International Conference on Program Comprehension.
         </Col>
       </Row>
     </Container>
---
+++ src/Abstract_.tsx
@@ -0,0 +1,68 @@
+import Container from "react-bootstrap/Container";
+import Figure from "react-bootstrap/Figure";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Abstract() {
+  return (
+    <Container style={{ marginTop: "2rem" }}>
+      <Row>
+        <Col md="auto">
+          <Figure>
+            <Figure.Image width={200} alt="Kisub Kim" src="./profile.jpeg" />
+          </Figure>
+        </Col>
+        <Col>
+          <h2>Kisub Kim</h2>
+          <h4>Research Scientist at Singapore Management University</h4>
+          Dr. Kisub Kim is a research scientist (postdoc) at{" "}
+          <a href="https://www.smu.edu.sg">
+            Singapore Management University (SMU)
+          </a>
+          . He is mainly working on software engineering, specifically application of artificial intelligence under the supervision of{" "}
+          <a href="http://www.mysmu.edu/faculty/davidlo/">Prof. David Lo</a>.
+          Before joining SMU, he was a research associate at the{" "}
+          <a href="https://wwwen.uni.lu/">University of Luxembourg</a>. He completed
+          his Ph.D. at the{" "}
+          <a href="https://wwwen.uni.lu/">University of Luxembourg</a> under
+          the supervision of{" "}
+          <a href="https://bissyande.github.io/">
+            Prof. Dr. Tegawendé F. BISSYANDE
+          </a>
+          ,{" "}
+          <a href="https://www.darkrsw.net/">
+            Prof. Dr. Dongsun Kim
+          </a>
+          ,{" "}
+          <a href="https://jacquesklein2302.github.io/">
+            Prof. Dr. Jacques Klein
+          </a>
+          , and{" "}
+          <a href="https://wwwfr.uni.lu/snt/people/yves_le_traon">
+            Prof. Dr. Yves Le Traon
+          </a>
+          . He has worked for{" "}
+          <a href="https://www.khmc.or.kr/">
+            Kyung Hee University Hospital
+          </a>{" "}
+          as a developer. He received his M.Eng. at {" "}
+          <a href="http://www.chungbuk.ac.kr">
+            Chungbuk National University
+          </a>{" "}
+          under the supervision of{" "}
+          <a href="https://scholar.google.com.hk/citations?user=U7Tc7gEAAAAJ&hl=en">Prof. Dr. Woosung Jung</a>. He
+          received B.Eng. in{" "}
+          <a href="https://computer.chungbuk.ac.kr/">Computer Engineering Department</a>{" "}
+          at <a href="hwww.chungbuk.ac.kr">Chungbuk National University</a> under
+          the supervision of{" "}
+          <a href="http://nlp.cbnu.ac.kr/">
+            Prof. Dr. Younghun Seo
+          </a>
+          . His main research domain include code search, bug localization, and code representation.
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Abstract;
