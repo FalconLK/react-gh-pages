92a2a56540aa9fa9e67265c4e00c7aee3443e399
Falcon <falcon@SMUs-MacBook-Pro.local>
2023-04-15 02:01:07 +0800

    deploy

---
+++  jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  globals: {
+    // This is necessary because next.js forces { "jsx": "preserve" }, but ts-jest appears to require { "jsx": "react" }
+    'ts-jest': {
+      tsconfig: {
+        jsx: 'react',
+      },
+    },
+  },
+};
