5c160ed8025d67837f7a03bf3986d118c39746be
Falcon <falcon@SMUs-MacBook-Pro.local>
2023-04-15 01:32:37 +0800

    Updates

---
+++ precache-manifest.27470ffd66befc155769708a50933fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6b2ffaf6b09fa882757",
+    "url": "/react-gh-pages/static/css/main.6a07343e.chunk.css"
+  },
+  {
+    "revision": "b6b2ffaf6b09fa882757",
+    "url": "/react-gh-pages/static/js/main.4fe6673b.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "b50878da168b103f01a9",
+    "url": "/react-gh-pages/static/css/2.d3b20674.chunk.css"
+  },
+  {
+    "revision": "b50878da168b103f01a9",
+    "url": "/react-gh-pages/static/js/2.461a4c07.chunk.js"
+  },
+  {
+    "revision": "a7c90da6d625db6b73aee68de835e52e",
+    "url": "/react-gh-pages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bb7151073529faeead6466946fc2afd1.js"
+  "/react-gh-pages/precache-manifest.27470ffd66befc155769708a50933fd5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
