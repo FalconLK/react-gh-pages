22ddacb1612fda859bec9d4df366cf5534913748
Falcon <falcon@SMUs-MacBook-Pro.local>
2023-09-05 20:29:58 +0800

    Updates

---
+++ precache-manifest.03893e52c22ee804587eec8c8bdae547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e42830d67beccc8d5fe",
+    "url": "/react-gh-pages/static/css/main.2291bff9.chunk.css"
+  },
+  {
+    "revision": "2e42830d67beccc8d5fe",
+    "url": "/react-gh-pages/static/js/main.2e42830d.chunk.js"
+  },
+  {
+    "revision": "ec9c29ee23614a06f3c1",
+    "url": "/react-gh-pages/static/css/1.3963ee31.chunk.css"
+  },
+  {
+    "revision": "ec9c29ee23614a06f3c1",
+    "url": "/react-gh-pages/static/js/1.ec9c29ee.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "934d5a80d9d71e25196c59e2216383d4",
+    "url": "/react-gh-pages/index.html"
+  }
+];
