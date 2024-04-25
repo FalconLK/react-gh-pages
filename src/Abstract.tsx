a8baa9e5f4d0bc50a3bd55b74d01b069a0f82060
Falcon <falcon@Falcons-MacBook-Pro.local>
2024-04-25 17:41:33 +0800

    deploy

--- src/Abstract.tsx
+++ src/Abstract.tsx
@@ -20,7 +20,7 @@ function Abstract() {
             Singapore Management University (SMU)
           </a>
           {/*. My work is mainly related to Software Engineering, broadly AI4SE and SE4AI. Specifically, I put my efforts on code generation, code search, bug localization, code review, and code representation under the supervision of{" "}*/}
-          . Within the dynamic landscape of Software Engineering, I navigate the fascinating realms of AI4SE and SE4AI. My focus encompasses the intricate domains of code generation, search, bug localization, review, and representation, all guided by the invaluable mentorship of {" "}.
+          . Within the dynamic landscape of Software Engineering, I navigate the fascinating realms of AI4SE and SE4AI. My focus encompasses the intricate domains of source code generation, search, review, representation, and bug related tasks, all guided by the invaluable mentorship of {" "}
           <a href="http://www.mysmu.edu/faculty/davidlo/">Prof. David Lo</a>.
           Before joining SMU, I was a research associate at the{" "}
           <a href="https://wwwen.uni.lu/">University of Luxembourg (UL)</a>. I completed
@@ -35,8 +35,9 @@ function Abstract() {
           </a>
           .
           {"\n"}
-          My works were presented at major conferences such as <b>International Conference on Software Engineering</b> and published in top journals such as <b>Empirical Software Engineering</b>. 
-          I have served as a reviewer, program committee, and organization committee in various software engineering conferences and journals, such as IEEE Transactions on Software Engineering, Empirical Software Engineering, and IEEE/ACM International Conference on Program Comprehension.
+          {/*My works were presented at major conferences such as <b>International Conference on Software Engineering (ICSE)</b> and published in top journals such as <b>Empirical Software Engineering (EMSE)</b>.*/}
+          {/*I have served as a reviewer, program committee, and organization committee in various software engineering conferences and journals, such as <b>Transactions on Software Engineering (TSE)</b> and <b>International Conference on Program Comprehension (ICPC)</b>.*/}
+          My research has been showcased at prestigious conferences like the <b>International Conference on Software Engineering (ICSE)</b> and featured in esteemed journals such as <b>Empirical Software Engineering (EMSE)</b>. Additionally, I've contributed to the scholarly community by fulfilling roles as a reviewer, program committee member, and organizer for prominent software engineering conferences and journals, including <b>Transactions on Software Engineering (TSE)</b> and <b>International Conference on Program Comprehension (ICPC)</b>.
         </Col>
       </Row>
     </Container>
